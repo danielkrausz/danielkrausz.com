ba784a5f2c35647c58ab5df958d421d85359da80
danielkrausz <dnielkrausz@gmail.com>
2023-01-24 00:32:39 +0100

    WIP: Add blog posting

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,18 @@
+import { LoaderFunction, useLoaderData } from "remix";
+import invariant from "tiny-invariant";
+import { getPost } from "~/posts";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, "missing params.slug");
+  return getPost(params.slug);
+};
+
+export default function PostSlug() {
+  const post = useLoaderData();
+  return (
+    <div>
+      <h1>Post: {post.title} </h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  );
+}
